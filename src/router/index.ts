b0318e496000f74ace78e5ef9b6001757c776db8
Cream260 <64160260@go.buu.ac.th>
2024-02-14 23:10:50 +0700

    PackagePage

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/Package',
+      name: 'Package',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PackageView.vue')
     }
   ]
 })
