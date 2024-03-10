b723cd57054c1dbe50784571faadd86a7c1fb6eb
Saharat Sawangarrom <64160149@go.buu.ac.th>
2024-03-10 21:53:52 +0700

    edit fillpack

--- src/views/Fill_Package.vue
+++ src/views/Fill_Package.vue
@@ -90,7 +90,7 @@ const saveOrder = async ()=>{
         </v-row>
         <v-row class="ml-8">
 
-          <v-col cols="12" lg="6">
+          <!-- <v-col cols="12" lg="6">
             <v-flex>
 
               <input type="text" placeholder="ราคารวม" class="placeholder-color forumSize0"
@@ -101,14 +101,14 @@ const saveOrder = async ()=>{
                 v-model="price" />
 
             </v-flex>
-          </v-col>
+          </v-col> -->
           <v-col cols="12" lg="6">
             <v-flex>
               <input type="text" placeholder="ส่วนลด" class="placeholder-color forumSize0"
                 v-model="orderStore.currentOrder.discount" />
             </v-flex>
           </v-col>
->>>>>>> 8b1698212fd83681d5ee0b9dafb119da0c259a90
+
         </v-row>
         <v-row class="ml-8">
           <v-col cols="12" lg="12">
