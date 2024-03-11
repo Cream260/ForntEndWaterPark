6d3a8e6ea56e5aedd7563337574bbb2d1b873f03
Aftertnw <64160049@go.buu.ac.th>
2024-03-12 03:05:15 +0700

    finish validation at ticket

--- src/stores/order.store.ts
+++ src/stores/order.store.ts
@@ -174,7 +174,7 @@ export const useOrderStore = defineStore("order", () => {
       }
 
 
-      if (ThChildqty.value > 0 && ThAdultqty.value === 0 ) {
+      if ((ThChildqty.value || EnChildqty.value ) > 0 && (ThAdultqty.value || EnAdultqty.value) === 0 ) {
         await Swal.fire({
           title: "กรุณาเลือกบัตรให้ถูกต้อง!",
           text: `"กรุณาเลือกจำนวนบัตรผู้ใหญ่ถ้ามีบัตรเด็ก" `,
@@ -184,16 +184,16 @@ export const useOrderStore = defineStore("order", () => {
         console.log("เลือกตั๋ว");
         return; // Exit function early if qty is 0
       }
-      if (EnChildqty.value > 0 && EnAdultqty.value === 0 ) {
-        await Swal.fire({
-          title: "กรุณาเลือกบัตรให้ถูกต้อง!",
-          text: `"กรุณาเลือกจำนวนบัตรผู้ใหญ่ถ้ามีบัตรเด็ก" `,
-          icon: "warning",
-          showCloseButton: true,
-        });
-        console.log("เลือกตั๋ว");
-        return; // Exit function early if qty is 0
-      }
+      // if (EnChildqty.value > 0 && EnAdultqty.value === 0 ) {
+      //   await Swal.fire({
+      //     title: "กรุณาเลือกบัตรให้ถูกต้อง!",
+      //     text: `"กรุณาเลือกจำนวนบัตรผู้ใหญ่ถ้ามีบัตรเด็ก" `,
+      //     icon: "warning",
+      //     showCloseButton: true,
+      //   });
+      //   console.log("เลือกตั๋ว");
+      //   return; // Exit function early if qty is 0
+      // }
       const res = await orderService.saveOrder(order);
       currentOrder.value = res.data;
       console.log("currentOrder", currentOrder.value);
